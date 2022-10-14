1af890fd0c305062048ab54aaf26e2a199169308
François <francois@oniti.fr>
2022-10-14 10:15:36 +0200

    mise en place version

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+)
+
+var Version = "1.0.0"
 
 func main() {
 	port := flag.Int("p", 8080, "port to listen on")
 	serverTomlFile := flag.String("c", "", "server config file")
+	dumpVersion := flag.Bool("v", false, "dump version")
 
 	flag.Parse()
+
+	if *dumpVersion {
+		println(Version)
+		return
+	}
 
 	if *port == 0 {
 		panic("Port must be specified")
@@ -19,6 +30,8 @@ func main() {
 	s := NewServer(*port, *serverTomlFile)
 
 	go listenSignals(s)
+
+	fmt.Println("Version : ", Version)
 
 	s.Start()
 }
